141edbf49e559f93ebafdab80c1e6d09c62a89ee
GedTest <viktorzwinger@seznam.cz>
2024-02-06 09:51:02 +0100

    create Navbar comp

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <h2>Zahradní skřítek</h2>
+            <div className="links">
+                <a href="#">Kalendář</a>
+                <a href="#">Plánek zahrady</a>
+                <a href="#">Archiv</a>
+            </div>
+        </nav>
+    );
+}
